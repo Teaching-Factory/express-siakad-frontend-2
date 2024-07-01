6ff17f6279a61727408abbc41ba011c6b995bc1b
aidaandinar8 <aidaandinar8@gmail.com>
2024-07-01 11:48:17 +0700

    dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -20,7 +20,7 @@
             </div>
         </div>
         <div class="col-12 xl:col-8">
-            <div class="row ml-1 mb-1">
+            <div class="row">
                 <div class="col-8">
                     <span>Jadwal Hari Ini (Rabu, 06 Maret 2024)</span>
                 </div>
@@ -30,7 +30,7 @@
             </div>
             <div class="card bg-theme">
                 <div class="row">
-                    <div class="col-12 xl:col-7">
+                    <div class="col-12 xl:col-8 yl:col-8">
                         <div class="text-danger">
                             <span>Saat ini Anda tidak memiliki jadwal kuliah yang harus diikuti, tetaplah semangat belajar dan menghasilkan karya - karya yang bermanfaat bagi nusa dan bangsa.</span>
                         </div>
@@ -38,18 +38,43 @@
                             <span>Atau Anda bisa akses halaman berikut : </span>
                         </div>
                     </div>
-                    <div class="col-12 xl:col-5 d-flex justify-content-center align-items-center">
+                    <div class="col-12 xl:col-4 d-flex justify-content-center align-items-center">
                         <img src="../assets/images/gambar.jpg" alt="Gambar" class="img-fluid custom-img" />
                     </div>
                 </div>
             </div>
-            <div class="card">
-                <div class="flex align-items-center justify-content-between mb-4">
-                    <h5>Notifications</h5>
+            <div class="grid">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="card">
+                            <span>Jadwal Hari Ini (Rabu, 06 Maret 2024)</span>
+                            <div class="flex align-items-center justify-content-between mb-4">
+                                <h5>Notifications</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="card">
+                            <span>Jadwal Hari Ini (Rabu, 06 Maret 2024)</span>
+                            <div class="flex align-items-center justify-content-between mb-4">
+                                <h5>Notifications</h5>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="card">
+            <div class="row">
+                <div class="col-12">
+                    <span>Noti</span>
+                    <div class="flex align-items-center justify-content-between mb-4">
+                        <h5>Notifications</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
 </template>
 
 <style scoped>
