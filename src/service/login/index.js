fef788020b1f79f310b7be53465c0b86fe9461be
akuutauf <taufikhidayatbwx09121@gmail.com>
2024-10-02 10:59:12 +0700

    Menamnbahkan Chapta Awal

--- src/service/login/index.js
+++ src/service/login/index.js
@@ -1,25 +1,26 @@
-import { setPermissions, setSettingGlobal, setToken, setUser } from "../../utiils/local_storage";
-import { post } from "../../utiils/request";
-export const login = async (username, password) => {
+import { setPermissions, setSettingGlobal, setToken, setUser } from '../../utiils/local_storage';
+import { post } from '../../utiils/request';
+export const login = async (username, password, captchaToken) => {
     try {
         const body = {
             username: username,
-            password: password
-        }
+            password: password,
+            captchaToken: captchaToken
+        };
 
         const res = await post('auth/do-login', body, {
             withoutAutorization: true
         });
 
-        if(res.status === 200) {
+        if (res.status === 200) {
             const { data } = res;
             setToken(data.token);
             setUser(data.user);
             setPermissions(data.permissions);
-            setSettingGlobal(data.setting_global_prodi)
+            setSettingGlobal(data.setting_global_prodi);
         }
         return res;
     } catch (error) {
         throw error;
     }
-}
+};
